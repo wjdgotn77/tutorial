ded7fb27b963fd6f99d98fb256e3ef96a8891500
haesoo <wjdgotn77@gmail.com>
2022-04-07 17:10:58 +0900

    TIL : module.css 사용방법

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import styles from '../styles/NavBar.module.css';
+
 export default function NavBar() {
   // path관련 정보를 얻을 수 있따.
   const router = useRouter();
@@ -8,10 +10,16 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === '/' ? 'red' : 'blue' }}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === '/' ? styles.active : ''
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === '/about' ? 'red' : 'blue' }}>
+        <a className={router.pathname === '/about' ? styles.active : ''}>
           About
         </a>
       </Link>
@@ -21,3 +29,24 @@ export default function NavBar() {
 
 // Link 태그 내에는 style, className 등의 속성을 지정할 수 없다
 // Link 로 감싸고 내부에 a 태그를 사용해준다.
+
+// css module 을 import 한다.
+// className = nav 로 준다고 해서 해당 모듈 내의 .nav 스타일이 적용되지 않는다.
+// className 에 해당 module의 styles.nav 이런식으로 props 상속받듯이 적어주어야 한다.
+// 자바스크립트의 오브젝트에서 프로퍼티 형식으로 적는것처럼!
+{
+  /* <nav>
+<Link href="/">
+  <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+</Link>
+<Link href="/about">
+  <a className={router.pathname === '/about' ? styles.active : ''}>
+    About
+  </a>
+</Link>
+</nav> */
+}
+
+// 두가지의 className을 함께 쓰려면
+// 1. {`${첫번째} ${두번째}`}
+// 2. {[첫번째, 두번째].join(" ")}
